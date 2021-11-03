94496c9417bac5271251f97124e9782d31265928
evguevarra <evguevarra08@gmail.com>
2021-11-03 12:43:08 +0800

    created item cards for menu

--- src/controller/CashierMainController.java
+++ src/controller/CashierMainController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class CashierMainController {
+
+    @FXML
+    private ImageView addOnsIconImage;
+
+    @FXML
+    private ImageView beverageIconImage;
+
+    @FXML
+    private ImageView dessertIconImage;
+
+    @FXML
+    private Label employeeName;
+
+    @FXML
+    private ImageView foodIconImage;
+
 }
---
+++ src/controller/CashierMenuItemsController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class CashierMenuItemsController {
+
+    @FXML
+    private AnchorPane addToOrderBtn;
+
+    @FXML
+    private Label menuItemName;
+
+    @FXML
+    private Label menuItemPrice;
+
+}
