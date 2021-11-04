d5c8ece06d12a2c2ddef22acc9faf732869e01cc
evguevarra <evguevarra08@gmail.com>
2021-11-04 17:27:23 +0800

    created manager main panel

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -5,11 +5,13 @@ import com.jfoenix.controls.JFXRadioButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -18,9 +20,11 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
-
+public class LoginController implements Initializable {
+    private String empPosition;
     @FXML
     private JFXRadioButton cashierRB;
 
@@ -42,6 +46,8 @@ public class LoginController {
     @FXML
     private TextField usernameField;
 
+    ToggleGroup rdGroup = new ToggleGroup();
+
     @FXML
     void handleExit(MouseEvent event) {
         Stage stage = (Stage) exitBtn.getScene().getWindow();
@@ -51,20 +57,16 @@ public class LoginController {
     @FXML
     void handleSignin(ActionEvent event) throws IOException {
 
-//        Parent root = FXMLLoader.load(LoginController.class.getResource("/views/CashierMainUI.fxml"));
-//        Scene scene = new Scene(root);
-//        Stage stage = new Stage();
-//        stage.setTitle("Welcome to FastFood Billing System");
-//        stage.setScene(scene);
-//        stage.initModality(Modality.NONE);
-//        stage.show();
-//        Stage previousStage = (Stage) exitBtn.getScene().getWindow();
-//        previousStage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/views/LoginLoadingUI.fxml"));
+        Parent root = fxmlLoader.load();
+        //Parent root = FXMLLoader.load(LoginController.class.getResource("/views/LoginLoadingUI.fxml"));
 
-        Parent root = FXMLLoader.load(LoginController.class.getResource("/views/LoginLoadingUI.fxml"));
+        LoginLoadingController loadingController = fxmlLoader.getController();
+        loadingController.setEmployeePosition(empPosition);
+
         Scene scene = new Scene(root);
         Stage stage = new Stage();
-        //stage.setTitle("Welcome to FastFood Billing System");
         stage.setScene(scene);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.initModality(Modality.NONE);
@@ -75,4 +77,21 @@ public class LoginController {
 
     }
 
+    public void getEmployeePosition(ActionEvent event){
+        if(managerRB.isSelected()){
+            empPosition = "manager";
+           // System.out.println(empPosition);
+        }else if(cashierRB.isSelected()){
+            empPosition = "cashier";
+           // System.out.println(empPosition);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        managerRB.setToggleGroup(rdGroup);
+        cashierRB.setToggleGroup(rdGroup);
+
+
+    }
 }
--- src/controller/ManagerMainController.java
+++ src/controller/ManagerMainController.java
@@ -1,4 +1,66 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
 public class ManagerMainController {
+
+    private String currentTab;
+
+    @FXML
+    private Pane employeeBtn;
+
+    @FXML
+    private ImageView employeeIconImage;
+
+    @FXML
+    private Pane employeeIndicator;
+
+    @FXML
+    private Label employeeName;
+
+    @FXML
+    private ImageView foodIconImage;
+
+    @FXML
+    private Pane menuBtn;
+
+    @FXML
+    private Pane menuIndicator;
+
+    @FXML
+    private Pane reportBtn;
+
+    @FXML
+    private ImageView reportIconImage;
+
+    @FXML
+    private Pane reportIndicator;
+    @FXML
+    void handleEmployeeBtn(MouseEvent event) {
+        currentTab = "employee";
+        System.out.println(currentTab);
+
+    }
+
+    @FXML
+    void handleMenuBtn(MouseEvent event) {
+        currentTab = "menu";
+//        Image image = new Image(getClass().getResourceAsStream("/img/restaurant-highlight.png"));
+//        foodIconImage.setImage(image);
+        System.out.println(currentTab);
+    }
+
+    @FXML
+    void handleReportsBtn(MouseEvent event) {
+        currentTab = "report";
+//        Image image = new Image(getClass().getResourceAsStream("/img/restaurant-highlight.png"));
+//        foodIconImage.setImage(image);
+        System.out.println(currentTab);
+    }
+
 }
