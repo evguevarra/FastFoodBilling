34cd50bc84397ad0a4b3d6a4c38821b1a4785b13
evguevarra <evguevarra08@gmail.com>
2021-11-09 09:44:10 +0800

    added imagechooser to dialogs

--- src/controller/ManagerEmployeeAddController.java
+++ src/controller/ManagerEmployeeAddController.java
@@ -1,11 +1,17 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,10 +21,34 @@ public class ManagerEmployeeAddController implements Initializable {
     private DatePicker birdatePicker;
 
     @FXML
+    private JFXButton browseBtn;
+
+    @FXML
+    private ImageView employeeImage;
+
+    @FXML
     private ComboBox<String> genderCBox;
 
     @FXML
     private ComboBox<String> positionCBox;
+
+    FileChooser fileChooser = new FileChooser();
+
+    @FXML
+    void handleBrowseButton(ActionEvent event) {
+        fileChooser.setTitle("Image Chooser");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().clear();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files","*.png","*.jpg","*.gif"));
+
+        File file = fileChooser.showOpenDialog(null);
+
+        if(file != null){
+            employeeImage.setImage(new Image(file.toURI().toString()));
+        }else{
+            System.out.println("A file is invalid");
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/controller/ManagerMenuAddController.java
+++ src/controller/ManagerMenuAddController.java
@@ -1,11 +1,17 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,12 +20,35 @@ public class ManagerMenuAddController implements Initializable {
     @FXML
     private ComboBox<String> categoryCBox;
 
-    //private String[] category = {"Food","Beverage","Dessert","Add-on"};
+    @FXML
+    private JFXButton browseBtn;
+
+    @FXML
+    private ImageView menuImage;
+
+    FileChooser fileChooser = new FileChooser();
+
+    @FXML
+    void handleBrowseBtn(ActionEvent event) {
+        fileChooser.setTitle("Image Chooser");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().clear();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files","*.png","*.jpg","*.gif"));
+
+        File file = fileChooser.showOpenDialog(null);
+
+        if(file != null){
+            menuImage.setImage(new Image(file.toURI().toString()));
+        }else{
+            System.out.println("A file is invalid");
+        }
+    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         categoryCBox.setItems(FXCollections.observableArrayList("Food","Beverage","Dessert","Add-on"));
 
-        //categoryCBox.getItems().addAll();
+
     }
 }
