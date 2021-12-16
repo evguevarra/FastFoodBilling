a82a69d7e6ca6d0eaedc72b6a8402fd1bb0031a8
evguevarra <evguevarra08@gmail.com>
2021-12-16 15:12:44 +0800

    line graph generating

--- src/controller/ManagerReportsController.java
+++ src/controller/ManagerReportsController.java
@@ -1,4 +1,137 @@
 package controller;
 
-public class ManagerReportsController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Menu;
+import model.Sales;
+import repositories.DatabaseConnection;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ManagerReportsController implements Initializable {
+
+    Connection connection = DatabaseConnection.connect();
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    @FXML
+    private TableColumn<Sales, String> dateColumn;
+
+    @FXML
+    private DatePicker fromDate;
+
+    @FXML
+    private LineChart lineChart;
+
+    @FXML
+    private TableView<Sales> salesTable;
+
+    @FXML
+    private DatePicker toDate;
+
+    @FXML
+    private TableColumn<Sales, Double> totalColumn;
+
+    ObservableList<Sales> observableList = FXCollections.observableArrayList();
+
+    private void displayTable(){
+        try {
+            String query = "SELECT * FROM sales";
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+                String salesDate = resultSet.getString("date");
+                double salesTotal = Double.parseDouble(resultSet.getString("totalSales"));
+
+
+                observableList.add(new Sales(salesDate,salesTotal));
+            }
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        totalColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
+
+
+        salesTable.setItems(observableList);
+
+    }
+
+    @FXML
+    void handleGenerate(ActionEvent event) {
+
+
+
+//        series.getData().add(new XYChart.Data("Feb",20));
+//        series.getData().add(new XYChart.Data("March",15));
+//        series.getData().add(new XYChart.Data("April",18));
+//        series.getData().add(new XYChart.Data("May",16));
+//        series.getData().add(new XYChart.Data("June",23));
+//        series.getData().add(new XYChart.Data("July",15));
+//        series.getData().add(new XYChart.Data("Aug",19));
+//        series.getData().add(new XYChart.Data("Sep",21));
+//        series.getData().add(new XYChart.Data("Oct",24));
+//        series.getData().add(new XYChart.Data("Nov",20));
+//        series.getData().add(new XYChart.Data("Dec",25));
+
+
+    }
+
+    @FXML
+    void handleReset(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        displayTable();
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Sales");
+
+        ArrayList<String> dates = new ArrayList<String>();
+        ArrayList<Double> total = new ArrayList<Double>();
+
+        try {
+            String query = "SELECT * FROM sales";
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+                dates.add(resultSet.getString("date"));
+                total.add(Double.parseDouble(resultSet.getString("totalSales")));
+            }
+            preparedStatement.close();
+
+            for(int i = 0; i < dates.size();i++){
+                series.getData().add(new XYChart.Data(dates.get(i),total.get(i)));
+            }
+
+            lineChart.getData().add(series);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
